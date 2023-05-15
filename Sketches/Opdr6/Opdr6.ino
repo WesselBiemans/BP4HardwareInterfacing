6a14b1291835ad859b82edaa2cc8f87ca9579c1d
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2023-05-15 14:00:44 +0200

    Componenten toegevoegd en opdr 6 verbeterd

--- Sketches/Opdr6/Opdr6.ino
+++ Sketches/Opdr6/Opdr6.ino
@@ -1,30 +1,30 @@
 void setup()
 {
 	//ROOD
-	pinMode(13, OUTPUT);
+	pinMode(11, OUTPUT);
   
   	//BLAUW
-  	pinMode(12, OUTPUT);
+  	pinMode(10, OUTPUT);
   
   	//GROEN
-  	pinMode(11, OUTPUT);
+  	pinMode(9, OUTPUT);
 }
 
 void loop()
 {
-  //13
-  kleur(255, 0, 0);
-  //13 12
-  kleur(255, 255, 0);
-  //13 12 11
-  kleur(255, 255, 255);
-  //13 11
-  kleur(255, 0, 255);
   //11
+  kleur(255, 0, 0);
+  //11 10
+  kleur(255, 255, 0);
+  //11 10 9
+  kleur(255, 255, 255);
+  //11 9
+  kleur(255, 0, 255);
+  //9
   kleur(0, 0, 255);
-  //11 12
+  //9 10
   kleur(0, 255, 255);
-  //12
+  //10
   kleur(0, 255, 0);
   //reset
   kleur(0, 0, 0);
@@ -33,8 +33,8 @@ void loop()
 //De reden waarom het RBG is i.p.v. RGB is omdat ik dan input 13, 12 en 11 kan houden.
 //(Makkelijker om mee te werken)
 void kleur (int Rood, int Blauw, int Groen){
-  analogWrite(13, Rood);
-  analogWrite(11, Groen);
-  analogWrite(12, Blauw);
+  analogWrite(11, Rood);
+  analogWrite(9, Groen);
+  analogWrite(10, Blauw);
   delay(1000);
 }
